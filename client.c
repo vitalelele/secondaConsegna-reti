85c3d9a5a47df0077f0a1a2730b933c0d34792f1
Antonio Vitale <65810505+vitalelele@users.noreply.github.com>
2022-12-18 12:51:49 +0100

    Add files via upload
    Programma terminato e commenti aggiunti.

---
+++ client.c
@@ -0,0 +1,153 @@
+/* Sviluppato con cura da Antonio Vitale [754740], Angelo Sciarra[758256], Antonio Troncellito[754736] */
+
+#if defined WIN32 // se l'OS è win32 includiamo la libreria <winsock.h>
+    #include <winsock.h>
+#else // se l'OS fa parte della famiglia UNIX
+    #define closesocket close
+    #include <sys/socket.h>
+    #include <arpa/inet.h>
+    #include <unistd.h>
+#endif // questo if defined controlla che l'OS sia win32 piuttosto che UNIX
+
+// librerie standard per ambo gli OS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h> 
+#define ECHOMAX 255
+
+void ClearWinSock(){
+	#if defined WIN32
+	WSACleanup();
+	#endif
+}
+
+// Funzione che ritorna 1 se il carattere passato è una vocale, 0 altrimenti
+int isVocale(char car){
+	char vocali[] = {'a', 'e', 'i', 'o', 'u'};
+	int i;
+	for(i=0; i<5; i++){
+		if(car == vocali[i]){
+			return 1;
+		}
+	}
+	return 0;
+}
+
+int main(){
+    #if defined WIN32
+        WSADATA wsaData; // inizializziamo un elemento WSADATA per essere sicuri che le socket windows siano supportate dal sistema
+        // MAKEWORD(2,2) specifica il numero di winsock sul sistema
+        int iResult = WSAStartup(MAKEWORD(2 ,2), &wsaData);
+        if (iResult != 0) {
+            printf ("error at WSASturtup\n");
+            return EXIT_FAILURE;
+        }
+    #endif
+
+	char nomeServer[30];
+	int numeroPorta;
+
+	// Inserimento nome server da input
+	printf("Inserisci il nome del server: ");
+	scanf("%19s", nomeServer);
+
+	// Inserimento numero porta da input 
+	printf("Inserisci il numero della porta: ");
+	scanf("%d", &numeroPorta);
+
+	// Traduzione del nome server in indirizzo internet (metodo gethostbyname()) attraverso DNS
+	struct hostent *serverAddr;
+	struct in_addr addr;
+
+	serverAddr = gethostbyname(nomeServer);
+
+	if(serverAddr == NULL){
+        printf("Impossibile tradurre il nome. Verra automaticamente inserito l'hostname: localhost\n");
+        char *nomeServerDefault = "localhost";
+        serverAddr = gethostbyname(nomeServerDefault);
+	}
+    int i = 0;
+    if(serverAddr -> h_addrtype == AF_INET){
+        while(serverAddr -> h_addr_list[i] != 0){
+            addr.s_addr = *(u_long *) serverAddr -> h_addr_list[i++];
+        }
+    }
+
+    // Creazione della socket
+    // int socket (int famigliaProtocolli, int tipoDiSocket, int portocolloDaUsare)
+	// nel nostro caso PF_INET = Internet Protocol Family, SOCK_DGRAM poichè è una socket UDP, IPPROTO_UDP)	
+    int sock;
+    if((sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0){
+        printf("Creazione della socket fallita.");
+        return -1;
+    }
+
+    // Creazione dell'indirizzo del server
+    struct sockaddr_in serverUDPaddr;
+    struct sockaddr_in fromAddr;
+    memset(&serverUDPaddr, 0, sizeof(serverUDPaddr));
+    serverUDPaddr.sin_family = PF_INET;
+  	// chiediamo all'utente di inserire la porta del server da contattare
+	// se ne inserisce una errata utilizziamo una di default
+    if(numeroPorta != 9069){
+        printf("La porta inserita non e' corretta, verra' automaticamente inserita la porta 9069...\n");
+        numeroPorta = 9069;
+    }
+    serverUDPaddr.sin_port = htons(numeroPorta);
+    serverUDPaddr.sin_addr.s_addr = addr.s_addr;
+
+    // invio della stringa di benvenuto al server
+    char *stringaBenvenuto = "Ciao, io sono il client!";
+    int lunghezzaStringa = strlen(stringaBenvenuto);
+    if(sendto(sock, stringaBenvenuto, lunghezzaStringa, 0, (struct sockaddr*)&serverUDPaddr, sizeof(serverUDPaddr)) != lunghezzaStringa){
+        printf("La sendto() ha fallito.\n");
+        ClearWinSock();
+        return -1;
+    }
+
+    // Ricevo stringa di conferma dal server
+    int fromSize;
+    char buffer[ECHOMAX];
+    fromSize = sizeof(fromAddr);
+    memset(buffer,0,ECHOMAX);
+    int bufferSize = recvfrom(sock, buffer, ECHOMAX, 0, (struct sockaddr*)&fromAddr, &fromSize);
+    if(serverUDPaddr.sin_addr.s_addr != fromAddr.sin_addr.s_addr){
+        printf("Errore! Il pacchetto ricevuto proviene da un'altra macchina (IP: %s).\n", inet_ntoa(fromAddr.sin_addr));
+        return -1;
+    }
+    printf("%s\n", buffer);
+
+    // Chiedo all'utente la stringa da tradurre
+    char strClient[60];
+    printf("Inserisci la stringa da inviare al server: ");
+    while(getchar() != '\n');
+    fgets(strClient,59,stdin);
+
+    // Ricezione (recv()) e invio (send()) delle vocali
+    printf("Caratteri ricevuti: ");
+    for(i=0; i<strlen(strClient); i++){
+        char carattere;
+        carattere = tolower(strClient[i]);
+        if(isVocale(carattere) == 1){
+            if(sendto(sock, &carattere, sizeof(char), 0, (struct sockaddr*)&serverUDPaddr, sizeof(serverUDPaddr)) != sizeof(char)){
+                printf("Errore nell'invio del carattere.\n");
+            }
+            recvfrom(sock, &carattere, sizeof(char), 0, (struct sockaddr*)&fromAddr, &fromSize);
+            if(serverUDPaddr.sin_addr.s_addr == fromAddr.sin_addr.s_addr){
+                printf("%c ", carattere); // stampa del carattere
+            }
+        }
+    }
+    printf("\n");
+    // Invio di un carattere '$' che ci permette di interrompere la connessione con il server (viene controllato server side)
+    char carattereFine = '$';
+    if(sendto(sock, &carattereFine, sizeof(char), 0, (struct sockaddr*)&serverUDPaddr, sizeof(serverUDPaddr)) != sizeof(char)){
+        printf("Errore nell'invio del carattere.\n");
+    }
+
+    system("pause");
+    closesocket(sock);
+
+    return 0;
+    
+} // end main()
---
+++ server.c
@@ -0,0 +1,117 @@
+/* Sviluppato con cura da Antonio Vitale [754740], Angelo Sciarra[758256], Antonio Troncellito[754736] */
+
+#if defined WIN32 // se l'OS è win32 includiamo la libreria <winsock.h>
+    #include <winsock.h>
+#else // se l'OS fa parte della famiglia UNIX
+    #define closesocket close
+    #include <sys/socket.h>
+    #include <arpa/inet.h>
+    #include <unistd.h>
+#endif // questo if defined controlla che l'OS sia win32 piuttosto che UNIX
+
+// librerie standard per gli OS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h> 
+
+#define PORTA_SERVER 9069  // definiamo la porta server da utilizzare
+#define MAX_CONNESSIONI 5 // definiamo un limite massimo di connessioni
+#define BUFFERSIZE 512   // definiamo la grandezza del buffer come costante (BUFFERSIZE)
+#define ECHOMAX 255	    // definiamo la lunghezza MAX di una stringa che si può mandare (MAXSTR) abbiamo scelto 255 perché è
+					   // più o meno la metà del buffersize (512)
+
+
+void ClearWinSock(){
+    #ifdef WIN32
+        WSACleanup;
+    #endif
+}
+
+void ErrorHandler(char *errorMessage) {
+	printf(errorMessage);
+}
+
+
+int main(int argc,char *argv[]){
+	#if defined WIN32
+	WSADATA wsaData;  // inizializziamo un elemento WSADATA per essere sicuri che le socket windows siano supportate dal sistema
+    // MAKEWORD(2,2) specifica il numero di winsock sul sistema
+	int iResult = WSAStartup(MAKEWORD(2 ,2), &wsaData);
+	if (iResult != 0) {
+		printf ("Errore nell'inizializzazione di WSAStartup()\n");
+		return EXIT_FAILURE;
+	}
+	#endif
+
+	int sock;
+	struct sockaddr_in serverAddr;
+	struct sockaddr_in clientAddr;
+	unsigned int cliAddrLen;
+	// creazione socket UDP
+	if((sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0){
+		printf("Creazione della socket fallita.\n");
+	}
+
+	// costruzione indirizzo server
+	memset(&serverAddr, 0, sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(PORTA_SERVER);
+	// poichè il server non contatta nessuno questo indirizzo sarà sempre localhost (127.0.0.1)
+	serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+     	
+	// assegnamo la porta e l'ip alla socket e verifichiamo se non ci sono errori
+	// int bind (int socket, struct sockaddr* localaddress, int addressLength)
+	// bind ritorna 0 in caso di successo e -1 altrimenti
+	if((bind(sock, (struct sockaddr*)&serverAddr, sizeof(serverAddr))) < 0){
+		printf("Il metodo bind() ha fallito.\n");
+	}
+	char buffer[ECHOMAX]; // creiamo il nostro buffer
+	
+	// Questo while ci permette di rimanere in ascolto anche se il client cade.
+	while(1){
+		int clientAddrLen = sizeof(clientAddr);
+
+        // void * memset( void *buffer, int c, size_t count )
+		// copia il valore di "c" nell'area di memoria (puntata da una variabile di tipo Puntatore ) 
+		// per una quantità stabilita nell'argomento count
+		memset(&buffer,0,sizeof(buffer));
+		recvfrom(sock, buffer, ECHOMAX, 0, (struct sockaddr*)&clientAddr, &clientAddrLen);
+		printf("%s", buffer);
+
+		// Traduzione da IP a hostname del client attraverso il DNS (metodo gethostbyaddr())
+		struct hostent *host;
+		host = gethostbyaddr((char *) &clientAddr.sin_addr, 4, AF_INET);
+
+        printf(" Messaggio ricevuto dal client %s\n\n", host->h_name);
+
+        // Invio risposta al messaggio di benvenuto del client
+		char *rispostaBenvenuto = "OK";
+		if( sendto(sock, rispostaBenvenuto, strlen(rispostaBenvenuto), 0 ,(struct sockaddr*)&clientAddr, clientAddrLen) != strlen(rispostaBenvenuto) ){
+			printf("Errore nell'invio rispostaBenvenuto.\n'");
+		}
+
+        // Ricezione e invio delle vocali
+		char vocaleRicevuta = ' '; 
+		struct sockaddr_in fromAddr;
+		int fromSize = sizeof(fromAddr);
+		while(1){
+            recvfrom(sock, &vocaleRicevuta,sizeof(char),0,(struct sockaddr*)&fromAddr, &fromSize);
+           // controlliamo che la recvfrom() vada a buon fine, eventualmente avvisiamo l'utente del problema
+		    if(clientAddr.sin_addr.s_addr != fromAddr.sin_addr.s_addr){
+                printf("Vocale ricevuta da un'altro client, IP: %s",inet_ntoa(fromAddr.sin_addr));
+                vocaleRicevuta = ' ';
+            }
+            // Controlliamo che la vocale ricevuta non sia l'ultima (carattere '$' definito in client come flag d'uscita)
+            if(vocaleRicevuta != '$'){
+               vocaleRicevuta = toupper(vocaleRicevuta);
+                if(sendto(sock, &vocaleRicevuta, sizeof(char), 0, (struct sockaddr*)&clientAddr, sizeof(clientAddr)) != sizeof(char)){
+                    printf("Errore nell'invio del carattere.\n");
+                }
+            }else{
+                break;
+            }
+		}
+	}
+	closesocket(sock); // chiudiamo la socket e terminiamo il programma
+} // end main()
